89586995ccfd290e1fe074865364ce67ecaca3d7
Partika <matheusorzechowski@gmail.com>
2024-08-07 17:40:34 -0300

    add leitura O D K

--- 1910.py
+++ 1910.py
@@ -1,1 +1,21 @@
 #aqui será gerada a solução do problema
+
+def validacoes():
+  print()
+
+def leitura():
+  dados = input().split()
+  O, D, K = map(int, dados)
+  return O, D, K
+
+def main():
+  while(True):
+    O, D, K = leitura()
+    if(O == 0, O == D, O == K):
+      break
+    
+  print("-1")
+
+grafo = []
+
+main()
