e730e43db83f81d4f7135ccdadea7db863f02034
Partika <matheusorzechowski@gmail.com>
2024-08-07 17:48:19 -0300

    add leitura proibidos

--- 1910.py
+++ 1910.py
@@ -3,14 +3,22 @@
 def validacoes():
   print()
 
-def leitura():
+def leitura_ODK():
   dados = input().split()
   O, D, K = map(int, dados)
   return O, D, K
 
+def leitura_proibidos(K:int):
+  dados = input().split()
+  proibidos = [int(dado) for dado in dados]
+  if(len(proibidos)!=K):
+    print("?")
+  return proibidos
+
 def main():
   while(True):
-    O, D, K = leitura()
+    O, D, K = leitura_ODK()
+    proibidos = leitura_proibidos(K)
     if(O == 0, O == D, O == K):
       break
     
